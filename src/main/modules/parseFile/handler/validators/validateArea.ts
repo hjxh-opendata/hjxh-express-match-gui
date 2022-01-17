2a5e0b3428a47571623a16975703ebf7052cbc8f
MarkShawn2020 <shawninjuly@gmail.com>
2022-01-18 00:08:02 +0800

    Upgrade the validate-area algo, supporting the big citis

--- src/main/modules/parseFile/handler/validators/validateArea.ts
+++ src/main/modules/parseFile/handler/validators/validateArea.ts
@@ -1,60 +1,53 @@
 import { GenericError } from '../../../../base/interface/errors';
-import { assert } from '../../../../base/interface/utils';
-import { ErrorValidateInvalid, ErrorValidators } from '../../interface/errors/validatingRoes';
+import { ErrorValidateArea } from '../../interface/errors/validatingRoes';
 
 export const PROVINCE_LIST = [
-  '湖南',
-  '安徽',
-  '浙江',
-  '广东',
-  '湖北',
-  '江西',
-  '上海',
-  '江苏',
-  '北京',
-  '河北',
-  '河南',
-  '天津',
-  '福建',
-  '山东',
-  '广西',
-  '山西',
-  '贵州',
-  '四川',
-  '重庆',
-  '云南',
-  '海南',
+  ['湖南'],
+  ['安徽'],
+  ['浙江'],
+  ['广东', '深圳'],
+  ['湖北'],
+  ['江西'],
+  ['上海'],
+  ['江苏'],
+  ['北京'],
+  ['河北'],
+  ['河南'],
+  ['天津'],
+  ['福建'],
+  ['山东'],
+  ['广西'],
+  ['山西'],
+  ['贵州'],
+  ['四川'],
+  ['重庆'],
+  ['云南'],
+  ['海南'],
 
-  '陕西',
-  '黑龙江',
-  '吉林',
-  '辽宁',
+  ['陕西'],
+  ['黑龙江'],
+  ['吉林'],
+  ['辽宁'],
 
-  '甘肃',
-  '宁夏',
-  '内蒙古',
+  ['甘肃'],
+  ['宁夏'],
+  ['内蒙古'],
 
-  '青海',
-  '新疆',
-  '西藏',
+  ['青海'],
+  ['新疆'],
+  ['西藏'],
 ];
-export const PROVINCE_CHARS = Array.from(new Set(PROVINCE_LIST.join(''))).join('');
 
-export const validateArea = (area: string): string => {
-  try {
-    const N = area.length;
-    let temp = '';
-    assert(N >= 2, 'area is at least 2 words');
-    for (let i = 0; i < N; i += 1) {
-      if (PROVINCE_CHARS.includes(area[i])) {
-        assert(i + 1 < N, `cannot ensure what province this address belongs to: ${area}`);
-        if (PROVINCE_LIST.includes((temp = area[i] + area[i + 1]))) return temp;
-        assert(i + 2 < N, `cannot ensure what province this address belongs to: ${area}`);
-        if (PROVINCE_LIST.includes((temp = area[i] + area[i + 1] + area[i + 2]))) return temp;
-      }
+export const validateArea = (inputArea: string): string => {
+  const area = inputArea.trim().slice(0, 5);
+  // eslint-disable-next-line no-restricted-syntax
+  for (const cities of PROVINCE_LIST) {
+    if (cities.some((city) => area.includes(city))) {
+      return cities[0];
     }
-    throw new Error(`cannot ensure what province this address belongs to: ${area}`);
-  } catch (e) {
-    throw new GenericError<ErrorValidators>(ErrorValidateInvalid, (e as unknown as Error).message);
   }
+  throw new GenericError(
+    ErrorValidateArea,
+    `cannot ensure what province this address belongs to: ${inputArea}`
+  );
 };
--- src/main/modules/parseFile/interface/errors/validatingRoes.ts
+++ src/main/modules/parseFile/interface/errors/validatingRoes.ts
@@ -8,11 +8,24 @@ export type ErrorValidateNotNull = typeof ErrorValidateNotNull;
 export const ErrorValidateDuplicate = 'ErrorValidateDuplicate';
 export type ErrorValidateDuplicate = typeof ErrorValidateDuplicate;
 
-export const ErrorValidateInvalid = 'ErrorValidateInvalid';
-export type ErrorValidateInvalid = typeof ErrorValidateInvalid;
+export const ErrorValidateArea = 'ErrorValidateArea';
+export type ErrorValidateArea = typeof ErrorValidateArea;
+
+export const ErrorValidateWeight = 'ErrorValidateWeight';
+export type ErrorValidateWeight = typeof ErrorValidateWeight;
+
+export const ErrorValidateDate = 'ErrorValidateDate';
+export type ErrorValidateDate = typeof ErrorValidateDate;
+
+export const ErrorValidateFee = 'ErrorValidateFee';
+export type ErrorValidateFee = typeof ErrorValidateFee;
 
 export const errorValidators = [
-  ErrorValidateInvalid,
+  ErrorValidateWeight,
+  ErrorValidateDate,
+  ErrorValidateArea,
+  ErrorValidateFee,
+
   ErrorValidateDuplicate,
   ErrorValidateNotNull,
 ] as const;
